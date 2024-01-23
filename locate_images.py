41558eca79c206c5fad930259d275cfc48eda1d9
XYCode <xycode-xyc@outlook.com>
2024-01-23 17:02:13 +0800

    结构修改

--- locate_images.py
+++ locate_images.py
@@ -5,5 +5,7 @@ taskbar_icon = pathlib.Path('./images_to_locate/taskbar_icon.png').absolute()
 username_input = pathlib.Path('./images_to_locate/login/username_input.png').absolute()
 password_input = pathlib.Path('./images_to_locate/login/password_input.png').absolute()
 login_confirm_button = pathlib.Path('./images_to_locate/login/confirm.png').absolute()
-next_question_button = pathlib.Path('./images_to_locate/next_question.png').absolute()
-audio_icon = pathlib.Path('./images_to_locate/audio.png').absolute()
+
+# 中英单词互译题
+transition_next_question_button = pathlib.Path('./images_to_locate/transition/next_question.png').absolute()
+transition_audio_icon = pathlib.Path('./images_to_locate/transition/audio.png').absolute()
